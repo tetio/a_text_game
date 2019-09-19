b13f0ed6737b86818f72a5262efae2e48b86ff7c
tetio <tetio@live.com>
2019-09-19 08:13:27 +0200

    unused files removed

--- lib/cat.ex
+++
@@ -1,9 +0,0 @@
-defmodule Cat do
-  require Record
-  Record.defrecord :cat, name: nil, breed: nil, age: 0
-  #efrecord M3U8, program_id: nil, path: nil, bandwidth: nil, ts_files: []
-
-  @type cat :: record(:cat, name: String.t, breed: String.t, age: integer)
-end
-
-
--- lib/some_data.ex
+++
@@ -1,7 +0,0 @@
-defmodule SomeData do
-  require Record
-  Record.defrecord :user, name: "john", age: 25
-
-  @type user :: record(:user, name: String.t, age: integer)
-  # expands to: "@type user :: {:user, String.t, integer}"
-end
